c7c4028654b10009984d53c0a3130834efdc0050
Nazmul Hossain <22538602+nazmulfx@users.noreply.github.com>
2024-11-10 10:27:37 +0600

    Update Vue.js

--- Vue.js
+++ Vue.js
@@ -10,15 +10,30 @@ npm run dev
 
 
 ##################################################
-############  Binding Text : v-bind ##############
+############  Binding Text : v-text ##############
 ##################################################
 {{ variable }}
-    - bind text for a portion
+    - bind text for a portion in a line
 
-<span v-bind="varible" ></span>
-    - bind
+<span v-text="varible" ></span>
+    - bind text for full line
 
-
+<template>
+<h1>{{ greet }} {{ name }}</h1>
+<h1 v-text="full_line"></h1>
+</template>
+<script>
+export default {
+    name: "App", 
+    data() {
+        return {
+            greet: "Hello,",
+            name: "Nazmul",
+            full_line: 'this is a full line of text',
+        }
+    }
+}
+</script>
 
 
 
